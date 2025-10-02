c0cef08dde3d8b99202c6e43138ac111db62b8ab
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-10-02 21:00:40 +0530

    session-06

--- mongodb.sh
+++ mongodb.sh
@@ -20,7 +20,7 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
   if [ $1 -ne 0 ]; then 
-    echo  -e  "$2  ...  $R FAILURE $N" | tee -a $LOG_FILE
+    echo  -e  "$2  ...  $R FAILURE  $N" | tee -a $LOG_FILE
     exit 1
   else 
     echo  -e "$2 ... $G SUCCESS $N"    | tee -a $LOG_FILE
@@ -33,7 +33,7 @@ VALIDATE $? "Adding Mongo repo"
 dnf install mongodb-org -y  &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB"
 
-systemctl enable monod &>>$LOG_FILE
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod
