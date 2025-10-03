a363921616b373e6f5d48818283455fa0e5a5920
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-10-03 17:32:46 +0530

    session-06

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -14,7 +14,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0  | cut -d "." -f1 )
 MONGODB_HOST=mongodb.daws86pavan.cyou 
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"  #var/log/shell-script/16-logs.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"  #var/log/shell-script/16-logs.log
 SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "script started ececuted at: $(date)" | tee -a $LOG_FILE
