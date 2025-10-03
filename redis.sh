b059c7432b9161b41d357de58d123193db4f2f3b
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-10-03 19:18:32 +0530

    session-06

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0  | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"  #var/log/shell-script/16-logs.log
-
+START_TIME=$(date +%s)
 mkdir -p $LOGS_FOLDER
 echo "script started ececuted at: $(date)" | tee -a $LOG_FILE
 
@@ -42,3 +42,6 @@ VALIDATE $? " Enabling Redis"
 systemctl start redis  &>>$LOG_FILE
 VALIDATE $? "Starting Redis"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$ (( $END_TIME -  $START_TIME))
+echo -e "script ececuted in: $Y $TOTAL_TIME Seconds $N"
